4c5b250f04358ea54e42617279f67dc858c29f9f
Tim Jensen <timrjensen@gmail.com>
2021-09-21 00:19:04 +0200

    SearchView

--- packages/app/src/components/SearchView/SearchView.tsx
+++ packages/app/src/components/SearchView/SearchView.tsx
@@ -33,59 +33,61 @@ export const SearchView = function SearchView({ query }: Props) {
       </div>
       <div className="query-body">
         {query.result.map((doc: PostDocument | CommentDocument, i) => {
-          const mappedContent = a(regExp, doc.content);
+          const mappedContent = mapContent(regExp, doc.content);
 
           if (typeof mappedContent === "string")
-            return <div key={`search-result-${i}`}>{mappedContent} ...</div>;
+            return (
+              <div key={`search-result-${i}`}>{`${mappedContent}...`}</div>
+            );
           else
-            return mappedContent.map((content, k) => (
+            return (
               <div>
-                {typeof content === "string" ? content : <b>{content[1]}</b>}
+                {mappedContent.map((content) => [
+                  "...",
+                  content[0],
+                  <b>{content[1]}</b>,
+                  content.length === 3 ? content[2] : null,
+                  " ",
+                ])}
               </div>
-            ));
+            );
         })}
       </div>
     </section>
   );
 };
 
-function a(regExp: RegExp, content: string) {
-  const result: (string[] | string)[] = [];
+function mapContent(regExp: RegExp, content: string) {
+  const result: string[][] = [];
   let i = 0;
 
-  //for (const match of content.matchAll(/\.|$/g)) {
-  for (const match of content.matchAll(regExp)) {
+  for (const match of content.matchAll(/\.|$/g)) {
     if (match.index === undefined) continue;
 
     const k = match.index;
     let a = 0;
+    let rest = undefined;
 
-    console.log(
-      content.slice(content.lastIndexOf(".", k), content.indexOf(".", k) + 1)
-    );
-    //for (const match of content.slice(i, k).matchAll(regExp)) {
-    for (const match of content
-      .slice(content.lastIndexOf(".|^", k), content.indexOf(".|$", k) + 1)
-      .matchAll(regExp)) {
-      console.log(i, a, k, match.index, content.slice(i, match.index));
+    for (const match of content.slice(i, k).trimLeft().matchAll(regExp)) {
       if (match.index === undefined || !match.input) continue;
 
-      result.push([content.slice(i, match.index), match[0]]);
-      i = a = match.index + match[0].length;
+      result.push([match.input.slice(a, match.index), match[0]]);
+      rest = match.input.slice(match.index + match[0].length, k);
+      a = match.index + match[0].length;
     }
 
-    //result.push(content.slice(i, k));
+    if (rest) result[result.length - 1].push(rest);
 
-    //i = k;
+    i = k + 1;
   }
 
   // result might be empty if the query matched user or date. In that case return a fixed array.
   if (result.length === 0) return content;
-  console.log(result);
+  //console.log(result);
   return result;
 }
 
-a(
-  /lorem/gi,
+mapContent(
+  /(lorem)/gi,
   `Lorem ipsum dolor sit amet, consectetur lorem adipiscing elit. Morbi at lectus et quam feugiat porta sed nec tortor. Ut rutrum libero non mauris mattis, tincidunt tempus magna faucibus. Mauris vel rutrum urna. Suspendisse molestie nisl ut dolor cursus, id laoreet erat vehicula. Fusce viverra quam sed tellus congue viverra. Donec feugiat maximus velit, id varius nisl malesuada id. Mauris sit amet enim varius, sodales sem at, fermentum lorem. Sed eu placerat risus. Donec a purus bibendum, dignissim orci nec, sodales mauris. Quisque orci massa, tincidunt quis orci et, ullamcorper gravida purus. In hac habitasse platea dictumst. Sed ullamcorper leo dui, at consequat purus finibus sed.`
 );
