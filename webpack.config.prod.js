3509acbaea363f8c23a6545e0e75f27c3cf51cf0
Tim Jensen <timrjensen@gmail.com>
2021-10-06 02:54:45 +0200

    changed dist path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   entry: resolve(__dirname, "./packages/app/src/index.tsx"),
   output: {
-    path: resolve(__dirname, "./packages/app/dist"),
+    path: resolve(__dirname, "./dist"),
     filename: "bundle.js",
   },
   mode: "production",
